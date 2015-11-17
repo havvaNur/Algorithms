39cd4c576dc126f42b51c415eeea76506e40a1c6
havvaNur <h922011@gmail.com>
2015-11-17 00:02:12 -0600

    selection java

---
+++ selection.java
@@ -0,0 +1,16 @@
+package simple_algorithms;
+
+public class selectionSort {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int[] a = {7,3,1,9,10, 3};
+        sort(a);
+        for(int i= 0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+        
+    }
+    
+    
+}
